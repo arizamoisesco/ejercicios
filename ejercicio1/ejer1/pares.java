49f80e809d8a94b1575623de688dadc6c3f03595
JOHAN <JOHAN@JOHAN-PC>
2014-09-04 17:30:29 -0500

    trabajo terminado 2

---
+++ ejercicio1/ejer1/pares.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejer1;
+
+/**
+ *
+ * @author JOHAN
+ */
+public class pares {
+ 
+    public int pares (int i) {
+        int sum; 
+        sum = 0;
+        for (i = 1 ; i <= 100 ; i+2 ) {
+            sum = 2 + i ;
+            
+        }
+        
+    }
+}
